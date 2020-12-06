7da76a80840a2f35688127a29fca2ec8845a7668
sphelelemendu <58187228+sphelelemendu@users.noreply.github.com>
2020-12-06 15:21:58 +0200

    Working Version With File Processing

--- Development Test/Program.cs
+++ Development Test/Program.cs
@@ -1,12 +1,101 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+
 
 namespace Development_Test
 {
     class Program
     {
-        static Tuple<Dictionary<int, int>, bool> isDuplicate(Dictionary<int, int> frequencyMap)
+        public static void CreateFile()
+        {
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            string spreadSheetPath = "test2.xlsx";
+            File.Delete(spreadSheetPath);
+            FileInfo spreadsheeinfor = new FileInfo(spreadSheetPath);
+            ExcelPackage pack = new ExcelPackage(spreadsheeinfor);
+            var slots = pack.Workbook.Worksheets.Add("distributionSlots");
+            slots.Cells["A1"].Value = "Cell ID";
+            slots.Cells["B1"].Value = "Northing";
+            slots.Cells["C1"].Value = "Easting";
+            slots.Cells["D1"].Value = "Frequency";
+            for (int i = 0; i < 100; i++)
+            {
+                var somerandom = new Random();
+
+                slots.Cells["A" + (i).ToString()].Value = i;
+                slots.Cells["B" + (i).ToString()].Value = (somerandom.Next() * 50000);
+                slots.Cells["C" + (i).ToString()].Value = (somerandom.Next() * 50000);
+                slots.Cells["A" + (i).ToString()].Value = 0;
+            }
+            pack.Save();
+
+        }
+        public static void CreateSpreadSheet(int[] freqList, List<List<int>> list)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            string spreadSheetPath = "BestFrequencySlotsAllocations.xlsx";
+            File.Delete(spreadSheetPath);
+            FileInfo spreadsheeinfor = new FileInfo(spreadSheetPath);
+            ExcelPackage pack = new ExcelPackage(spreadsheeinfor);
+            var slots = pack.Workbook.Worksheets.Add("distributionSlots");
+            slots.Cells["A1"].Value = "Cell ID";
+            slots.Cells["B1"].Value = "Northing";
+            slots.Cells["C1"].Value = "Easting";
+            slots.Cells["D1"].Value = "Frequency";
+            int x = 0;
+            for (int i = 2; i < list.Count + 2; i++)
+            {
+                slots.Cells["A" + (i).ToString()].Value = x;
+                slots.Cells["B" + (i).ToString()].Value = list[x][0];
+                slots.Cells["C" + (i).ToString()].Value = list[x][1];
+                slots.Cells["D" + (i).ToString()].Value = freqList[x];
+
+
+                x++;
+            }
+            pack.Save();
+
+        }
+        public static List<List<int>> ReadExcel(string path)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var list = new List<List<int>>();
+
+            FileInfo existingFile = new FileInfo(path);
+            using (ExcelPackage pack = new ExcelPackage(existingFile))
+            {
+                ExcelWorksheet work = pack.Workbook.Worksheets[0];
+                int colCount = work.Dimension.End.Column;
+                int rowCount = work.Dimension.End.Row;
+                for (int i = 2; i <= rowCount; i++)
+                {
+                    List<int> temp = new List<int>();
+                    for (int j = 2; j <= 3; j++)
+                    {
+                        try
+                        {
+
+                            temp.Add(Int32.Parse(work.Cells[i, j].Value.ToString().Trim()));
+                        }
+
+                        catch (FormatException e)
+                        {
+                            Console.WriteLine($"Could not parse'{work.Cells[i, j].Value.ToString()}'");
+                        }
+                    }
+
+                    list.Add(temp);
+
+
+                }
+            }
+            return list;
+        }
+        public static Tuple<Dictionary<int, int>, bool> isDuplicate(Dictionary<int, int> frequencyMap)
         {
             var needsUpdate = false;
             var completeList = new List<int> { 110, 111, 112, 113, 114, 115 };
@@ -48,56 +137,22 @@ namespace Development_Test
         }
         static void Main(string[] args)
         {
-            List<List<int>> list = new List<List<int>>();
+
+            List<List<int>> list = null;
             Dictionary<int, Double> dict;
             List<Dictionary<int, Double>> containerList = new List<Dictionary<int, Double>>();
+            string filename = "";
+            Console.WriteLine("Please Enter file name :");
+            filename = Console.ReadLine();
+
             int[] frequencyLists;
-
-            list.Add(new List<int>() {//point 0
-               111660,800
-            });
-            list.Add(new List<int>() { //pont 1
-               537032, 184006
-            });
-            list.Add(new List<int>() {//point 2.
-              5371,1838843
-            });
-            list.Add(new List<int>() {
-             1,3
-            });
-            list.Add(new List<int>() {
-               206,664685
-            });
-            list.Add(new List<int>() {
-               537248,185016
-            });
-            list.Add(new List<int>() {
-               537250,185020
-            });
-            list.Add(new List<int>() {
-               537267,18
-            });
-            list.Add(new List<int>() {
-               69,183451
-            });
-            list.Add(new List<int>() {
-               0,184140
-
-            });
-            list.Add(new List<int>() {
-              56,184927
-
-            });
-            list.Add(new List<int>() {
-               537380,18
-
-            });
-            Console.WriteLine("These are all the points and their distances!");
+            list = ReadExcel(filename);
             for (int i = 0; i < list.Count; i++)
             {
                 dict = new Dictionary<int, double>();
                 var reff = list[i];// our reference
-                var x2 = reff[0];
+
+                var x2 = reff[0];//index out of range
                 var y2 = reff[1];
                 for (int j = 0; j < list.Count; j++)
                 {
@@ -108,7 +163,6 @@ namespace Development_Test
                     var distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
 
                     dict.Add(j, distance);
-
                 }
                 dict = dict.OrderBy(kvp => kvp.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
                 containerList.Add(dict);
@@ -117,10 +171,10 @@ namespace Development_Test
             //
             frequencyLists = new int[containerList[0].Count];
             var temp = 110;
-            for (int i = 0; i < containerList[9].Count; i++)
+            for (int i = 0; i < containerList[0].Count; i++)
             {
 
-                frequencyLists[containerList[9].ElementAt(i).Key] = temp;
+                frequencyLists[containerList[0].ElementAt(i).Key] = temp;
 
                 if (temp == 115)
                 {
@@ -133,9 +187,9 @@ namespace Development_Test
             }
             //initializations
 
-            Console.WriteLine("Frequency2 " + String.Join("; ", frequencyLists));
+            Console.WriteLine("initial frequency list ! " + String.Join("; ", frequencyLists));
             int controlvar = 0;
-            bool go = true;
+
 
 
             int[] freqlist2;
@@ -143,7 +197,7 @@ namespace Development_Test
             {
                 freqlist2 = (int[])frequencyLists.Clone();
 
-                
+
                 while (controlvar < containerList.Count)
                 {
                     var currentDictionary = containerList[controlvar];
@@ -156,31 +210,21 @@ namespace Development_Test
                     var tuple = isDuplicate(closestFive);
                     if (tuple.Item2)
                     {
-                        Console.WriteLine("Were fixed Something!");
+
                         for (int i = 0; i < tuple.Item1.Count; i++)
                         {
                             frequencyLists[(tuple.Item1).ElementAt(i).Key] = (tuple.Item1).ElementAt(i).Value;
                         }
                     }
-                    else
-                    {
-
-
-                    }
-
-
-         
                     controlvar++;
                 }
-                Console.WriteLine("frequencyList " + String.Join("; ", frequencyLists));
-              
-                Console.WriteLine("freqlist2 " + String.Join("; ", freqlist2));
+
             }
             while (!frequencyLists.SequenceEqual(freqlist2));
 
 
             //now let print out container list
-            Console.WriteLine("Frequency2 " + String.Join("; ", frequencyLists));
+            Console.WriteLine("updated frequency list ! " + String.Join("; ", frequencyLists));
             foreach (Dictionary<int, Double> currentList in containerList)
             {
                 for (int i = 0; i < currentList.Count; i++)
@@ -191,6 +235,7 @@ namespace Development_Test
                 //Console.WriteLine(String.Join("; ", currentList));
                 Console.WriteLine();
             }
+            CreateSpreadSheet(frequencyLists, list);
 
             Console.ReadLine();
         }
